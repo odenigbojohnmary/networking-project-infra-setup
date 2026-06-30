f80731ab19c373fe2a3f6673950aa6694ae8f56a
Johnmary Odenigbo <odenigbojohnmary@gmail.com>
2026-06-30 12:40:08 +0100

    added the compute resources

--- vars.tf
+++ vars.tf
@@ -31,7 +31,7 @@ variable "private_subnet_cidr" {
 variable "zone" {
   description = "GCP zone"
   type        = string
-  default     = "us-central1-a"
+  default     = "us-central1-b"
 }
 
 variable "instance_name" {
